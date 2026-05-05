edc1be68d5835def2d52bca54443f5432102580c
James <james.atttia@gmail.com>
2026-05-05 13:29:49 +0800

    refactor(models): set password nullable=true for google auth

--- fintech-api/src/main/java/com/fintechapp/fintech_api/model/User.java
+++ fintech-api/src/main/java/com/fintechapp/fintech_api/model/User.java
@@ -29,7 +29,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String password;
 
     @Column(name = "auth_provider", nullable = false)
